ad3a86543087a9b79148f89b9527e8d37993ca32
Viktor Szépe <viktor@szepe.net>
2020-09-06 06:15:10 +0200

    Rewrite in Bash
    Using a for loop

--- vultr_ping.sh
+++ vultr_ping.sh
@@ -1,48 +1,31 @@
-# Use sh to test Vultr ping speeds
+#!/bin/bash
+#
+# Use Bash to test Vultr ping speeds
 # it has 15 looking glass https://www.vultr.com/faq/
 
+declare -A VULTR_PING_SERVERS=(
+    [Tokyo]=hnd-jp-ping.vultr.com.
+    [Singapore]=sgp-ping.vultr.com.
+    [Amsterdam]=ams-nl-ping.vultr.com.
+    [Paris]=par-fr-ping.vultr.com.
+    [Frankfurt]=fra-de-ping.vultr.com.
+    [London]=lon-gb-ping.vultr.com.
+    [New_Jersey]=nj-us-ping.vultr.com.
+    [Chicago]=il-us-ping.vultr.com.
+    [Dallas]=tx-us-ping.vultr.com.
+    [Atlanta]=ga-us-ping.vultr.com.
+    [Los_Angeles]=lax-ca-us-ping.vultr.com.
+    [Miami]=fl-us-ping.vultr.com.
+    [Seattle]=wa-us-ping.vultr.com.
+    [Silicon_Valley]=sjo-ca-us-ping.vultr.com.
+    [Sydney]=syd-au-ping.vultr.com.
+    [Toronto]=tor-ca-ping.vultr.com.
+    [Seoul]=sel-kor-ping.vultr.com.
+)
+declare CITY_COUNTER="0"
 
-echo 01 Tokyo
-ping -q hnd-jp-ping.vultr.com -c 10 > vult_ping.txt
-echo =============================
-echo 02 Singapore
-ping -q sgp-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 03 Amsterdam
-ping -q ams-nl-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 04 Paris
-ping -q par-fr-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 05 Frankfurt
-ping -q fra-de-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 06 London
-ping -q lon-gb-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 07 New York
-ping -q nj-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 08 Chicago
-ping -q il-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 09 Dallas
-ping -q tx-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 10 Atlanta
-ping -q ga-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 11 Los Angeles
-ping -q lax-ca-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 12 Miami
-ping -q fl-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 13 Seattle
-ping -q wa-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 14 Silicon Valley
-ping -q sjo-ca-us-ping.vultr.com -c 10 >> vult_ping.txt
-echo =============================
-echo 15 Sydney
-ping -q syd-au-ping.vultr.com -c 10 >> vult_ping.txt
+for CITY in "${!VULTR_PING_SERVERS[@]}"; do
+    printf '%02d %s\n' "$((++CITY_COUNTER))" "${CITY//_/ }"
+    echo ping -q -c 10 "${VULTR_PING_SERVERS[${CITY}]}"
+    echo '============================='
+done >vultr-ping.txt
